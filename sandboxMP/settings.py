894fe29e15cbe82b7029a2bb119e04d2bfdc7954
RobbieHan <robbie_han@outlook.com>
2018-11-27 01:05:11 +0800

    change to mysql

--- sandboxMP/settings.py
+++ sandboxMP/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'sandboxMP.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sandboxMP',
+        'HOST': '127.0.0.1',
+        'USER': 'ddadmin',
+        'PASSWORD': '1234@abcd.com',
+        'PORT': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
